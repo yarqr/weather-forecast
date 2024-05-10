0f36e1c7d6ed49d0815a85ede98c1be4a3b89f0d
DaniilKK2006 <daniilkov100@gmail.com>
2024-05-10 12:55:56 +0300

    fix: rm useless draft

--- frontend/form/redirect.js
+++ frontend/form/redirect.js
@@ -9,16 +9,3 @@ cityForm.addEventListener("submit", (event) => {
   let city = serializeForm(cityForm);
   window.location.replace(`${window.location}` + `/city=${city}`);
 });
-// let btn = document.querySelector(".btn")
-// btn.addEventListener("click", () => {
-//     window.location.replace(`${window.location}` + `/city=Киров`)
-// });
-
-// event.preventDefault();
-//   let city = serializeForm(cityForm);
-//   let request = fetch("#", {
-//     method: "POST",
-//     body: JSON.stringify({
-//       cityName: city,
-//     }),
-//   });
