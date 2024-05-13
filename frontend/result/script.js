b48bb65ba9c7271a8eaa27c7b3915514b823107b
DaniilKK2006 <daniilkov100@gmail.com>
2024-05-13 20:29:03 +0300

    refactor: async function

--- frontend/result/script.js
+++ frontend/result/script.js
@@ -1,5 +1,5 @@
 let params = new URLSearchParams(document.location.search);
-let body = document.querySelector("body");
+let body = document.querySelector(".res-page");
 let returnButton = document.querySelector(".main__return-button");
 let city = params.get("city");
 let url = document.location.origin + `/api/get/weather/${city}`;
@@ -7,22 +7,24 @@ let url = document.location.origin + `/api/get/weather/${city}`;
 returnButton.addEventListener("click", () => {
   window.location.replace(window.location.origin);
 });
+async function req() {
+  let response = await fetch(url);
+  let result = await response.json();
+  returnButton.removeAttribute("hidden");
+  if (result !== null) {
+    body.insertAdjacentHTML(
+      "afterbegin",
+      `<section> ${city.toUpperCase()}</section> 
+        <section class = "main__temp">  ${Math.round(
+          result.temperature
+        )} °C </section>`
+    );
+  } else {
+    body.insertAdjacentHTML(
+      "afterbegin",
+      `<section> city not found </section>`
+    );
+  }
+}
 
-let response = fetch(url)
-  .then((response) => response.json())
-  .then((data) => {
-    if (data !== null) {
-      returnButton.removeAttribute("hidden");
-      body.insertAdjacentHTML(
-        "afterbegin",
-        `<section> ${city} </section> 
-        <section>  ${Math.round(data.temperature)} °C </section>`
-      );
-    } else {
-      returnButton.removeAttribute("hidden");
-      body.insertAdjacentHTML(
-        "afterbegin",
-        `<section> city not found </section>`
-      );
-    }
-  });
+await req();
