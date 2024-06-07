ea44d0e1dc79c8c350f0e76d5439e2965262218b
YarQR <rosya6721@gmail.com>
2024-06-07 16:12:57 +0300

    fix: weather was incomplete

--- backend/presentation/web/api/get/weather.py
+++ backend/presentation/web/api/get/weather.py
@@ -3,10 +3,19 @@ from fastapi import Request
 from backend.application.common.services.weather import WeatherService
 
 
-async def get_weather(city: str, request: Request) -> Optional[dict[str, float]]:
+async def get_weather(
+    city: str, request: Request
+) -> Optional[dict[str, float | int | str]]:
     weather = await cast(WeatherService, request.app.state.weather_service).get_by_city(
         city
     )
     if weather is not None:
-        return {"temperature": weather.temperature}
+        return {
+            "temperature": weather.temperature,
+            "feels_like": weather.feels_like,
+            "humidity": weather.humidity,
+            "wind_speed": weather.wind_speed,
+            "description": weather.description,
+            "country": weather.country,
+        }
     return None
