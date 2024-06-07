845cbc96b7dea536005f7904e49e8e2e62727484
DaniilKK2006 <daniilkov100@gmail.com>
2024-06-07 21:41:48 +0300

    feat: another parameters of weather

--- frontend/result/script.js
+++ frontend/result/script.js
@@ -4,19 +4,55 @@ let returnButton = document.querySelector(".main__return-button");
 let city = params.get("city");
 let url = document.location.origin + `/api/get/weather/${city}`;
 let resPage = document.querySelector(".res-page")
+
 returnButton.addEventListener("click", () => {
   window.location.replace(window.location.origin);
 });
+
+class Weather {
+  constructor(temperature, description, feels_like, country, humidity, wind_speed) {
+    this.temperature = temperature
+    this.descriprtion = description
+    this.feels_like = feels_like
+    this.country = country
+    this.humidity = humidity
+    this.wind_speed = wind_speed
+  }
+  match() {
+    switch (this.descriprtion) {
+      case 'Rain':
+        return "/assets/icons/free-icon-storm-578132.png"
+      case 'Clouds':
+        return "/assets/icons/free-icon-cloud-4814293.png"
+      case 'Thunderstorm':
+        return "/assets/icons/free-icon-storm-3026395.png"
+      case 'Clear':
+        return "/assets/icons/free-icon-sun-4814268.png"
+      case 'Mist':
+        return "/assets/icons/free-icon-fog-3750506.png"
+      case 'Haze':
+        return "/assets/icons/free-icon-fog-3750506.png"
+    }
+  }
+  generImg() {
+    if (this.country !== "UA") {
+      return `https://flagsapi.com/${this.country}/flat/64.png`
+    }
+    return `https://flagsapi.com/RU/flat/64.png`
+  }
+}
 async function req() {
   let txt = ``
   let response = await fetch(url);
   let result = await response.json();
   returnButton.removeAttribute("hidden");
   if (result !== null) {
-    txt = `<section> ${city.toUpperCase()}</section> 
+    let weather = new Weather(result.temperature, result.description, result.feels_like, result.country, result.humidity, result.wind_speed)
+    txt = `<section class = "main__res">${city.toUpperCase() + ' '.repeat(150)}<img src=${weather.generImg()} class = "main__res__flag"/></section> 
         <section class = "main__temp">  ${Math.round(
           result.temperature
-        )} °C </section>`;
+        )} °C   <img class = "main__temp__state-icon" src = ${weather.match()}/> </section>
+        <section class = "main__wind">wind speed: ${weather.wind_speed} m/s</section>`
   } else {
     txt = `<section> city not found </section>`;
   }
